d85938d290e27ac07a8c3f641fc650a8b3122a2b
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-18 23:58:36 -0400

    coupling

---
+++ lab-00-coupling/src/main/java/com/cydeo/loosely/BalanceManager.java
@@ -0,0 +1,17 @@
+package com.cydeo.loosely;
+
+import java.math.BigDecimal;
+
+public class BalanceManager {
+
+
+
+
+    public boolean checkout(Balance balance,BigDecimal checkoutAmount) {
+        BigDecimal balanceAmount = balance.getAmount();
+
+        return balanceAmount.subtract(checkoutAmount)
+                .compareTo(BigDecimal.ZERO) > 0;
+
+    }
+}
