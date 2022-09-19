c001879b19a844b58b5cff97e48b08503be54b88
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-19 00:01:41 -0400

    coupling

---
+++ lab-00-coupling/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,32 @@
+package com.cydeo;
+
+import com.cydeo.loosely.Balance;
+import com.cydeo.loosely.BalanceManager;
+import com.cydeo.loosely.CustomerBalance;
+import com.cydeo.loosely.GiftCardBalance;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+        UUID user = UUID.randomUUID();
+
+       Balance customerBalance = new CustomerBalance(user, BigDecimal.ZERO);
+       Balance giftCardBalance = new GiftCardBalance(user, BigDecimal.ZERO);
+
+        customerBalance.addBalance(new BigDecimal(150));
+        giftCardBalance.addBalance(new BigDecimal(120));
+
+//        BalanceService balanceService = new BalanceService(customerBalance, giftCardBalance);
+
+
+        BalanceManager balanceManager = new BalanceManager();
+
+
+
+        System.out.println(balanceManager.checkout(customerBalance,new BigDecimal(80)));
+        System.out.println(balanceManager.checkout(giftCardBalance,new BigDecimal(80)));
+    }
+
+}
